83b0211cc645a466378557a012e0169972ebb0b7
Vico Ervanda <vicoerv@gmail.com>
2020-08-22 15:10:18 +0800

    update footer nav

--- layouts/default.vue
+++ layouts/default.vue
@@ -11,4 +11,62 @@
 </template>
 
 <style>
+.nav-menu {
+  position: fixed;
+  right: 5%;
+  z-index: 9999;
+  top: 50%;
+  margin-top: -30px;
+  cursor: pointer;
+  height: 60px;
+  width: 58px;
+  padding: 20px;
+  transition: all 0.2s ease;
+}
+
+.nav-menu span.line {
+  width: 2px;
+  height: 70px;
+  background: #fff;
+  display: block;
+  position: absolute;
+  top: 20px;
+  transition: all 0.3s ease;
+}
+
+.nav-menu span.text {
+  text-transform: uppercase;
+  font-size: 10px;
+  letter-spacing: 2px;
+  display: block;
+  opacity: 0;
+  color: #fff;
+  transition: all 0.4s ease;
+  transform: rotate(-90deg) translateX(-40px);
+}
+
+.nav-menu span.line:nth-child(1) {
+  left: 10px;
+}
+
+.nav-menu span.line:nth-child(2) {
+  left: 28px;
+}
+
+.nav-menu:hover span.line {
+  transform: translateY(-50%);
+}
+
+.nav-menu:hover span.line:nth-child(1) {
+  height: 0;
+}
+
+.nav-menu:hover span.line:nth-child(2) {
+  height: 90px;
+}
+
+.nav-menu:hover span.text {
+  transform: rotate(-90deg) translateX(-80px);
+  opacity: 1;
+}
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -101,16 +101,17 @@
 
     <footer>
       <div class="background-image vs-div" data-speed="-.6" style="background-image: url('/Footer.jpg')"></div>
-      <div class="container h-full">
+      <div class="container h-full py-6">
         <div class="footer-text container">
           <div class="footer-items footer-item-1">
-            <div>
-              <div>Next</div>
-              <div>Wellness</div>
+            <div class="footer-nav">
+              <div class="nav-link">Next</div>
+              <div class="nav-title">Wellness</div>
+              <img src="/Next%20Arrow.svg" alt="">
             </div>
           </div>
           <div class="footer-items footer-item-2">
-            <div class="footer-item about columns">
+            <div class="footer-item about">
               <div class="about-title">
                 Samara Spa
               </div>
@@ -126,7 +127,7 @@
               </p>
 
               <p class="about-copy">
-                &copy; 2020 SamaraSpa. Website by Fleava
+                &copy; 2020 SamaraSpa. Website by Fl&eacute;ava
               </p>
             </div>
           </div>
@@ -292,65 +293,6 @@ footer .background-image {
 header {
   height: 100vh;
   position: relative;
-}
-
-.nav-menu {
-  position: fixed;
-  right: 5%;
-  z-index: 9999;
-  top: 50%;
-  margin-top: -30px;
-  cursor: pointer;
-  height: 60px;
-  width: 58px;
-  padding: 20px;
-  transition: all 0.2s ease;
-}
-
-.nav-menu span.line {
-  width: 2px;
-  height: 70px;
-  background: #fff;
-  display: block;
-  position: absolute;
-  top: 20px;
-  transition: all 0.3s ease;
-}
-
-.nav-menu span.text {
-  text-transform: uppercase;
-  font-size: 10px;
-  letter-spacing: 2px;
-  display: block;
-  opacity: 0;
-  color: #fff;
-  transition: all 0.4s ease;
-  transform: rotate(-90deg) translateX(-40px);
-}
-
-.nav-menu span.line:nth-child(1) {
-  left: 10px;
-}
-
-.nav-menu span.line:nth-child(2) {
-  left: 28px;
-}
-
-.nav-menu:hover span.line {
-  transform: translateY(-50%);
-}
-
-.nav-menu:hover span.line:nth-child(1) {
-  height: 0;
-}
-
-.nav-menu:hover span.line:nth-child(2) {
-  height: 90px;
-}
-
-.nav-menu:hover span.text {
-  transform: rotate(-90deg) translateX(-80px);
-  opacity: 1;
 }
 
 /* New Section */
