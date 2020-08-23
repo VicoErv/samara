6df4213f8e12b7adf1b27b23a0bc8c2ab86ca035
Vico Ervanda <vicoerv@gmail.com>
2020-08-23 19:51:20 +0800

    menu animation

--- layouts/default.vue
+++ layouts/default.vue
@@ -20,7 +20,7 @@
       <Nuxt/>
     </div>
 
-    <div class="nav-menu" @click="show = true">
+    <div :class="`nav-menu ${(show ? 'close' : '')}`" @click="show = !show">
       <span class="line"></span>
       <span class="line"></span>
       <span class="text">Menu</span>
@@ -53,75 +53,4 @@ export default {
 </script>
 
 <style>
-.menu-container {
-  position: fixed;
-  top: 0;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  overflow: hidden;
-}
-
-.menu-container.top {
-  z-index: 999;
-}
-
-.nav-menu {
-  position: fixed;
-  right: 5%;
-  z-index: 998;
-  top: 50%;
-  margin-top: -30px;
-  cursor: pointer;
-  height: 60px;
-  width: 58px;
-  padding: 20px;
-  transition: all 0.2s ease;
-}
-
-.nav-menu span.line {
-  width: 2px;
-  height: 70px;
-  background: #fff;
-  display: block;
-  position: absolute;
-  top: 20px;
-  transition: all 0.3s ease;
-}
-
-.nav-menu span.text {
-  text-transform: uppercase;
-  font-size: 10px;
-  letter-spacing: 2px;
-  display: block;
-  opacity: 0;
-  color: #fff;
-  transition: all 0.4s ease;
-  transform: rotate(-90deg) translateX(-40px);
-}
-
-.nav-menu span.line:nth-child(1) {
-  left: 10px;
-}
-
-.nav-menu span.line:nth-child(2) {
-  left: 28px;
-}
-
-.nav-menu:hover span.line {
-  transform: translateY(-50%);
-}
-
-.nav-menu:hover span.line:nth-child(1) {
-  height: 0;
-}
-
-.nav-menu:hover span.line:nth-child(2) {
-  height: 90px;
-}
-
-.nav-menu:hover span.text {
-  transform: rotate(-90deg) translateX(-80px);
-  opacity: 1;
-}
 </style>
